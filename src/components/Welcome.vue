35ef272744b4c5bf02187dd7a2e1e3dd037bd025
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-08 02:20:33 +0800

    feat: welcome component for home

---
+++ src/components/Welcome.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2 class="title">
+    I'm Fiqar 'Lbexp' Aprialim
+  </h2>
+  <p class="desc">
+    Currently pursuing my career as software engineer in a Makassar based software house.
+  </p>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Welcome',
+});
+</script>
+<style scoped lang="scss">
+.title {
+  font-size: 4rem;
+};
+
+.desc {
+  font-size: 2rem;
+}
+</style>
