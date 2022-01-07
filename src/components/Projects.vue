a1f6bfcda99e927be5b71e4d825683b80c9503dc
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-08 02:39:52 +0800

    feat: project component

---
+++ src/components/Projects.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2 class="title">
+    Projects I've contributed on,
+  </h2>
+  <p class="desc">
+    Currently pursuing my career as software engineer in a Makassar based software house.
+  </p>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Projects',
+});
+</script>
+<style scoped lang="scss">
+.title {
+  font-size: 3.5rem;
+  margin-bottom: 0.75rem;
+};
+
+.desc {
+  font-size: 1.5rem;
+}
+</style>
