8b16c82b3050c67a2818f49577c836f897db853f
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-08 01:43:27 +0800

    fix: element structure

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <Navbar />
-  <router-view/>
+  <div class="app">
+    <Header />
+    <main class="main">
+      <router-view/>
+    </main>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Navbar from '@/components/layouts/Navbar.vue';
+import Header from '@/components/layouts/Header.vue';
 
 export default defineComponent({
   components: {
-    Navbar,
+    Header,
+    // Navbar,
   },
 });
 </script>
+
+<style scoped lang="scss">
+.app {
+  padding: 0px 100px
+}
+
+.main {
+  padding: 80px 0px;
+}
+</style>
