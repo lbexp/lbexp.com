9bb092949cd10bc307793077db810c7e658b9922
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-09 02:59:34 +0800

    fix: import footer in app.vue

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,29 @@
     <main class="main">
       <router-view/>
     </main>
+    <Footer />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Header from '@/components/layouts/Header.vue';
+import Footer from './components/layouts/Footer.vue';
 
 export default defineComponent({
   components: {
     Header,
-    // Navbar,
+    Footer,
   },
 });
 </script>
 
 <style scoped lang="scss">
 .app {
-  padding: 0px 100px
+  padding: 0px 100px;
 }
 
 .main {
-  padding: 80px 0px;
+  padding: 80px 0px 40px 0px;
 }
 </style>
