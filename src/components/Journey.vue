0ef80689a8bfab8572976f1d6e033f43a5dae03f
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-08 19:55:17 +0800

    feat: journey section

--- src/components/Journey.vue
+++ src/components/Journey.vue
@@ -1,25 +1,74 @@
 <template>
   <h2 class="title">
-    My journey so far,
+    My journey so far:
   </h2>
-  <p class="desc">
-    Currently pursuing my career as software engineer in a Makassar based software house.
-  </p>
+  <div class="content">
+    <ItemJourney v-for="(item, key) in journeyData" :key="key" :data="{ index: key, ...item }" />
+  </div>
 </template>
+
 <script lang="ts">
 import { defineComponent } from 'vue';
+import ItemJourney from '@/components/ItemJourney.vue';
 
 export default defineComponent({
   name: 'Journey',
+  components: {
+    ItemJourney,
+  },
+  setup() {
+    const journeyData = [
+      {
+        title: 'Software Engineer (Full-time)',
+        subtitle: 'Upana Studio',
+        time: 'January 2022 - Present',
+        desc: 'Lorem ipsum dolor sit amet',
+      },
+      {
+        title: 'Frontend Web Programmer (Full-time)',
+        subtitle: 'Upana Studio',
+        time: 'March 2021 - January 2022 (~10 Months)',
+        desc: 'Lorem ipsum dolor sit amet',
+      },
+      {
+        title: 'Frontend Web Programmer (Part-time)',
+        subtitle: 'Upana Studio',
+        time: 'October 2019 - December 2019 (~3 Months)',
+        desc: 'Lorem ipsum dolor sit amet',
+      },
+      {
+        title: 'Frontend Web Programmer (Intern)',
+        subtitle: 'Upana Studio',
+        time: 'June 2019 - September 2019 (~3 Months)',
+        desc: 'Lorem ipsum dolor sit amet',
+      },
+      {
+        title: 'Bachelor\'s degree, Informatics Engineering',
+        subtitle: 'Universitas Hasanuddin',
+        time: '2015 - 2020 (~5 Years)',
+        desc: 'Lorem ipsum dolor sit amet',
+      },
+    ];
+
+    return {
+      journeyData,
+    };
+  },
 });
 </script>
+
 <style scoped lang="scss">
 .title {
   font-size: 3.5rem;
-  margin-bottom: 0.75rem;
+  margin-bottom: 3rem;
 };
 
-.desc {
-  font-size: 1.5rem;
+.content {
+  display: flex;
+  gap: 3rem;
+  flex: 1;
+  overflow: auto;
+  white-space: nowrap;
+  padding-bottom: 20px;
 }
 </style>
